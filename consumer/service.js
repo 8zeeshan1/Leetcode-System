3cab889836ae6db63f54b5129683b09eb6bc44c6
MOHD Zeeshan Quraishi <zq8281@gmail.com>
2026-05-12 21:44:45 +0530

    It is a subscriber which will take the messages from the kafka in actual it will be the websocket connection with the user's browser so that he can see what is the result of his solution's submission

---
+++ consumer/service.js
@@ -0,0 +1,25 @@
+const kafka = require("../primary_server/kafka/client");
+
+async function run() {
+  const consumer = await kafka.consumer({
+    groupId: "leetcode-group"
+  });
+
+  await consumer.connect();
+
+  await consumer.subscribe({
+    topic: "leetcode",
+    fromBeginning: false, // only new messages
+  });
+
+  console.log("Consumer is running and waiting...");
+
+  await consumer.run({
+    eachMessage: async ({ message }) => {
+      const value = message.value.toString();
+      console.log("Received:", value);
+    },
+  });
+}
+
+run();
