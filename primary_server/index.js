327463feec14a6a23eb13a4d36f5520f72a8f071
MOHD Zeeshan Quraishi <zq8281@gmail.com>
2026-05-12 21:48:15 +0530

    It is the main server on which the solution will be submitted and it will keep that code submitted by the user on the message queue and then the workers will take the submissions from there process them and produce the result and give it to kafka pub - sub system.

---
+++ primary_server/index.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const {createClient} = require("redis")
+const {kafka} = require("./kafka/client")
+
+const app = express()
+const PORT = 3000
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+const client = createClient();
+client.connect();
+
+app.post("/submit", (req, res)=>{
+    const {problemId, userId, code, language} = req.body;
+
+    client.lPush("submissions", JSON.stringify({problemId, userId, code, language}))
+    res.json({
+        message: "Problem Submitted"
+    })
+
+})
+// Kafka is a distributed coordination system it have lots of partitioning, multiple brokers and consumers someone needs to keep track of everything that someone is zookeeper.
+
+app.listen(PORT, ()=>{
+    console.log("Server started on PORT: ", PORT)
+})
+
